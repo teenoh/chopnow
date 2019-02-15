1f60411909ca2ab2ee9006501995fd7a83049d85
teenoh <ugbalaachinike@gmail.com>
2019-02-15 02:09:45 +0100

    create meal service

---
+++ api/utils/dummyData.js
@@ -0,0 +1,29 @@
+
+export default {
+    meals: [
+        {
+            id: 1,
+            name: "Fried Rice",
+            size: "Medium",
+            price: 450
+        },
+        {
+            id: 2,
+            name: "Jollof Rice",
+            size: "Large",
+            price: 550
+        },
+        {
+            id: 3,
+            name: "Okro",
+            size: "Medium",
+            price: 450
+        },
+        {
+            id: 4,
+            name: "Plantain and Beans",
+            size: "Small",
+            price: 150
+        },
+    ]
+}
