411018b25446c3631ee4c594832e8ce5655d5711
teenoh <ugbalaachinike@gmail.com>
2019-02-16 21:21:20 +0100

    update MealService code to es6

--- api/services/meal.service.js
+++ api/services/meal.service.js
@@ -2,17 +2,17 @@ import dummyData from "../utils/dummyData"
 import Meal from "../models/meal.model"
 
 const MealService = {
-    fetchAllMeals(){
+    fetchAllMeals: () => {
         return dummyData.meals.map((meal) => {
             const newMeal = new Meal();
             newMeal.id = meal.id
             newMeal.name = meal.name
-            newMeal.size = meal.size
+            newMeal.desc = meal.desc
             newMeal.price = meal.price
             return newMeal
         })
     },
-    addMeal(meal){
+    addMeal: (meal) => {
         const mealLen  = dummyData.meals.length
         const lastId = dummyData.meals[mealLen - 1].id
         const newId = lastId + 1
@@ -22,9 +22,12 @@ const MealService = {
         dummyData.meals.push(meal);
         return meal
     },
-    getAMeal(id){
+    getAMeal: (id) => {
         const meal = dummyData.meals.find(meal => meal.id == id)
         return meal || {}
+
+    },
+    updateAMeal:  (id) => {
 
     }
 }
