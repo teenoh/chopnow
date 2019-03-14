41660f53d5a5819b14d10555626d53fce3b3fc24
teenoh <ugbalaachinike@gmail.com>
2019-03-14 23:51:50 +0100

    add category to menumeal

--- api/models/menumeal.js
+++ api/models/menumeal.js
@@ -1,25 +1,35 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const MenuMeal = sequelize.define('MenuMeal', {
-    mealId: {
-      type: DataTypes.INTEGER,
-      onDelete: 'CASCADE',
-      references: {
-        model: 'Meals',
-        key: 'id',
-        as: 'mealId'
+  const MenuMeal = sequelize.define(
+    'MenuMeal',
+    {
+      mealId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Meals',
+          key: 'id',
+          as: 'mealId'
+        }
+      },
+      menuId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Menus',
+          key: 'id',
+          as: 'menuId'
+        }
+      },
+      category: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        defaultValue: 'breakfast',
+        values: ['breakfast', 'lunch', 'dinner']
       }
     },
-    menuId: {
-      type: DataTypes.INTEGER,
-      onDelete: 'CASCADE',
-      references: {
-        model: 'Menus',
-        key: 'id',
-        as: 'menuId'
-      }
-    },
-  }, {});
+    {}
+  );
   MenuMeal.associate = function(models) {
     // associations can be defined here
   };
